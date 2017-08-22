e6d25ec8966df2b71ccecb83746c0b158fafe77f
feiying777 <81628466@qq.com>
2017-08-22 08:46:47 +0800

    study drill

--- python2/ex33.py
+++ python2/ex33.py
@@ -1,15 +1,21 @@
-i = 0
+
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
-    
-    i = i + 1
-    print "Numbers now:", numbers
-    print "At the bottom i is %d" % i
-    
+def add_numbers(n):
+    i = 0
+    while i < n:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
+        i = i + 1
+        print "Numbers now:", numbers
+        print "At the bottom i is %d" % i
+
+print "How many numbers do you want to add? "
+n = int(raw_input("> "))
+
+add_numbers(n)
+        
 print "The Numbers: "
 
 for num in numbers:
